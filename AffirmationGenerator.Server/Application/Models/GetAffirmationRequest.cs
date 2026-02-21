1408be1ed742287ab05862ddf305097b206330d3
Temirkhan Amanzhanov <87667470+timok19@users.noreply.github.com>
2026-02-21 17:03:45 +0100

    Rename AffirmationLanguageCode to LanguageCode (#31)
    Rename the request property from AffirmationLanguageCode to LanguageCode and update its Description to enumerate available language codes via AffirmationLanguage constants. Update GetAffirmationQuery to use request.LanguageCode, rename MapLanguageCode parameter, fix error creation to use the new variable, and change GetAffirmation/Translate to return Result<string> (using Result.Success/Error) while adjusting the query composition accordingly. Client App.tsx updated to send languageCode as the query param, import HttpStatusCode from axios, and use HttpStatusCode.TooManyRequests for rate-limit checks. Also apply a minor whitespace formatting change in .husky/task-runner.json.

--- AffirmationGenerator.Server/Application/Models/GetAffirmationRequest.cs
+++ AffirmationGenerator.Server/Application/Models/GetAffirmationRequest.cs
@@ -1,9 +1,18 @@
 using System.ComponentModel;
+using AffirmationGenerator.Server.Domain;
 
 namespace AffirmationGenerator.Server.Application.Models;
 
 public sealed record GetAffirmationRequest
 {
-    [Description("Available languages: en (English), de (German), cs (Czech), fr (French)")]
-    public required string AffirmationLanguageCode { get; init; }
+    [Description(
+        $"""
+            Available language codes: 
+            {AffirmationLanguage.English} ({nameof(AffirmationLanguage.English)}), 
+            {AffirmationLanguage.German} ({nameof(AffirmationLanguage.German)}), 
+            {AffirmationLanguage.Czech} ({nameof(AffirmationLanguage.Czech)}), 
+            {AffirmationLanguage.French} ({nameof(AffirmationLanguage.French)}) 
+            """
+    )]
+    public required string LanguageCode { get; init; }
 }
