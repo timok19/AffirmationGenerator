0efe12c3c208d02f8590ad5c79e62be859180bc0
Temirkhan Amanzhanov <tima1199@gmail.com>
2026-03-07 15:28:58 +0100

    Fix naming

--- AffirmationGenerator.Server/Application/Extensions/HttpContextExtensions.cs
+++ AffirmationGenerator.Server/Application/Extensions/HttpContextExtensions.cs
@@ -4,7 +4,7 @@ public static class HttpContextExtensions
 {
     extension(HttpContext httpContext)
     {
-        public string? GetClientIpFromHeaderOrDefault(string headerName) =>
+        public string? GetClientIpAddress(string headerName) =>
             httpContext.Request.Headers.TryGetValue(headerName, out var clientIp) == false
                 ? httpContext.Connection.RemoteIpAddress?.ToString()
                 : clientIp;
